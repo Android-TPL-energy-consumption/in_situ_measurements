d202a088dbde01e68c0aac17cf847e2106f361db
Rémy Raes <contact@remyraes.com>
2022-10-06 16:30:38 +0200

    refactor: run scenarios sequentially
    We now list tested applications with associated scenarios, and run them a given
    number of times, one after another.

--- run_scenario.py
+++ run_scenario.py
@@ -12,17 +12,50 @@ from utils.application import TestedApplication
 LVPMSerialNo = 12431
 monsoon = LVPM.Monsoon()
 adb = "/opt/android-sdk/platform-tools/adb"
-runsCount = 5
+runsCount = 2
 applications = [
     TestedApplication("Amplitude", "scenarios/monitoring/amplitude.sh"),
     TestedApplication("Firebase", "scenarios/monitoring/firebase.sh"),
-    TestedApplication("Amplitude", "scenarios/monitoring/new_relic.sh")
+    TestedApplication("New Relic", "scenarios/monitoring/new_relic.sh")
 ]
 
 
-def thread_function():
+def run_all_experiments():
+    # Let user some time to boot phone
+    print("You can boot your phone now.")
+    # sleep(60)
+
+    # Push test scenarios to phone
+    print("\n==> Uploading test scenarios to phone...")
+    for app in applications:
+        subprocess.call("{} push {} /data/local/tmp".format(adb, app.scenario), shell=True)
+
+    # Run scenarios
+    for x in range(runsCount):
+        for app in applications:
+            print("\n==> Launching experiment n°{} with {} application".format(x, app.name))
+
+            # Launch scenario
+            threading.Thread(target=thread_function, args=(app.scenario,)).start()
+
+            # Start sampling
+            monsoon_engine.enableCSVOutput("{}_{}.csv".format(app.name, x))
+            thread = threading.Thread(target=start_sampling)
+            thread.start()
+
+            # Stop sampling after scenario is over
+            print("Waiting for scenario to end...")
+            thread.join()
+            print("Scenario is over.")
+
+            # Wait for phone to be reconnected to computer
+            sleep(4)
+
+
+def thread_function(scenariopath):
     print("Running scenario on phone...")
-    subprocess.call('{} shell nohup sh /data/local/tmp/scenario.sh'.format(adb), shell=True)
+    scenario = scenariopath.split("/")[-1]
+    subprocess.call('{} shell nohup sh /data/local/tmp/{}'.format(adb, scenario), shell=True)
     print("Scenario launched.")
 
 
@@ -36,7 +69,6 @@ def setup_monsoon():
     monsoon.fillStatusPacket()
     monsoon.setVout(4)
     engine = sampleEngine.SampleEngine(monsoon)
-    engine.enableCSVOutput("results.csv")
     engine.ConsoleOutput(False)
     monsoon.setUSBPassthroughMode(op.USB_Passthrough.Auto)
 
@@ -48,27 +80,5 @@ def setup_monsoon():
 
 # Prepare Monsoon tooling
 monsoon_engine = setup_monsoon()
-
-# Let user some time to boot phone
-print("You can boot your phone now.")
-sleep(60)
-
-# Deploy scenario on phone
-print("Deploying scenario on phone...")
-subprocess.call("{} push scenario.sh /data/local/tmp".format(adb), shell=True)
-print("Done.")
-
-# Launch scenario
-thread = threading.Thread(target=thread_function)
-thread.start()
-
-# Start sampling
-thread = threading.Thread(target=start_sampling)
-thread.start()
-
-# Stop sampling after scenario is over
-print("Waiting for scenario to end...")
-thread.join()
-print("Scenario is over.")
-
-print("Done.")
+run_all_experiments()
+exit(0)
