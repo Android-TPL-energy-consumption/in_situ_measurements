2c22c18e2b19883d7ce578ef17b929ec2b2572d4
Rémy Raes <contact@remyraes.com>
2022-10-06 12:48:59 +0200

    feat: leave some time to user to boot test phone

--- run_scenario.py
+++ run_scenario.py
@@ -1,5 +1,7 @@
 import subprocess
 import threading
+from time import sleep
+
 import Monsoon.LVPM as LVPM
 import Monsoon.sampleEngine as sampleEngine
 import Monsoon.Operations as op
@@ -32,6 +34,10 @@ def setup_monsoon():
 
 # Prepare Monsoon tooling
 monsoon_engine = setup_monsoon()
+
+# Let user some time to boot phone
+print("You can boot your phone now.")
+sleep(60)
 
 # Deploy scenario on phone
 print("Deploying scenario on phone...")
