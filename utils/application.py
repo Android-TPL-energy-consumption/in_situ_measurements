84c6b7618806e47b7da88627fc1c6f378d9ecddb
Remy Raes <raes.remy@gmail.com>
2022-10-07 15:57:10 +0200

    docs: add documentation to TestedApplication class

--- utils/application.py
+++ utils/application.py
@@ -1,4 +1,8 @@
 class TestedApplication:
+    """This is a payload class representing a tested application.
+
+    It carries name of the tested application, and relative path (from project root) to associated test scenario.
+    """
     def __init__(self, name, scenario):
         self.name = name
         self.scenario = scenario
