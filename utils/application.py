28ca6b17b3e74fd26f6cb92a0c33e197b0f0bbe1
Rémy Raes <contact@remyraes.com>
2023-04-18 09:38:31 +0200

    feat: add category field to application class

--- utils/application.py
+++ utils/application.py
@@ -4,8 +4,9 @@ class TestedApplication:
     It carries name of the tested application, relative path (from project root) to associated test scenario, said
     scenario run time (in seconds), test application package name and path to tested application's APK file.
     """
-    def __init__(self, name, scenario, duration, package_name, apk_path):
+    def __init__(self, name, category, scenario, duration, package_name, apk_path):
         self.name = name
+        self.category = category
         self.scenario = scenario
         self.duration = duration
         self.package_name = package_name
