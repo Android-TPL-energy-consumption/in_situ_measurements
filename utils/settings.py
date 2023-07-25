3ef1afdcfc518ba8f4365f26ee55f5852e31ebd9
Remy Raes <contact@remyraes.com>
2023-07-25 16:21:00 +0200

    fix: use both NewRelic scenarios

--- utils/settings.py
+++ utils/settings.py
@@ -15,24 +15,24 @@ adb = "/home/remy/Android/Sdk/platform-tools/adb"
 #adb = "/opt/android-sdk/platform-tools/adb"
 
 # Number of times all scenarios will be ran.
-runsCount = 1
+runsCount = 30
 
 # Tested applications.
 applications = [
-#    TestedApplication("Amplitude", "monitoring", "scenarios/monitoring/amplitude.sh", 30, "tpl.monitoring.amplitude", "apks/monitoring/amplitude.apk"),
-#    TestedApplication("Firebase", "monitoring", "scenarios/monitoring/firebase.sh", 30, "tpl.monitoring.firebase", "apks/monitoring/firebase.apk"),
-#    TestedApplication("NewRelic", "monitoring", "scenarios/monitoring/newrelic.sh", 30, "tpl.monitoring.newrelic", "apks/monitoring/newrelic.apk"),
-#    TestedApplication("nosdk", "monitoring", "scenarios/monitoring/mo_nosdk.sh", 30, "tpl.monitoring.template", "apks/monitoring/nosdk.apk"),
+    TestedApplication("Amplitude", "monitoring", "scenarios/monitoring/amplitude.sh", 30, "tpl.monitoring.amplitude", "apks/monitoring/amplitude.apk"),
+    TestedApplication("Firebase", "monitoring", "scenarios/monitoring/firebase.sh", 30, "tpl.monitoring.firebase", "apks/monitoring/firebase.apk"),
+    TestedApplication("NewRelic", "monitoring", "scenarios/monitoring/mo_newrelic.sh", 30, "tpl.monitoring.newrelic", "apks/monitoring/newrelic.apk"),
+    TestedApplication("nosdk", "monitoring", "scenarios/monitoring/mo_nosdk.sh", 30, "tpl.monitoring.template", "apks/monitoring/nosdk.apk"),
 
     TestedApplication("Admob", "advertising", "scenarios/advertising/admob.sh", 40, "tpl.ads.banner.admob", "apks/advertising/admob.apk"),
     TestedApplication("Chartboost", "advertising", "scenarios/advertising/chartboost.sh", 40, "tpl.ads.banner.chartboost", "apks/advertising/chartboost.apk"),
     TestedApplication("Max", "advertising", "scenarios/advertising/max.sh", 40, "tpl.ads.banner.max", "apks/advertising/max.apk"),
     TestedApplication("nosdk", "advertising", "scenarios/advertising/ad_nosdk.sh", 40, "tpl.ads.banner.template", "apks/advertising/nosdk.apk"),
 
-#    TestedApplication("Crashlytics", "crash_reporting", "scenarios/crash_reporting/crashlytics.sh", 20, "tpl.crashreporting.crashlytics", "apks/crash_reporting/crashlytics.apk"),
-#    TestedApplication("NewRelic", "crash_reporting", "scenarios/crash_reporting/newrelic.sh", 20, "tpl.crashreporting.newrelic", "apks/crash_reporting/newrelic.apk"),
-#    TestedApplication("ACRA", "crash_reporting", "scenarios/crash_reporting/acra.sh", 20, "tpl.crashreporting.acra", "apks/crash_reporting/acra.apk"),
-#    TestedApplication("nosdk", "crash_reporting", "scenarios/crash_reporting/cr_nosdk.sh", 20, "tpl.crashreporting.template", "apks/crash_reporting/nosdk.apk")
+    TestedApplication("Crashlytics", "crash_reporting", "scenarios/crash_reporting/crashlytics.sh", 20, "tpl.crashreporting.crashlytics", "apks/crash_reporting/crashlytics.apk"),
+    TestedApplication("NewRelic", "crash_reporting", "scenarios/crash_reporting/cr_newrelic.sh", 20, "tpl.crashreporting.newrelic", "apks/crash_reporting/newrelic.apk"),
+    TestedApplication("ACRA", "crash_reporting", "scenarios/crash_reporting/acra.sh", 20, "tpl.crashreporting.acra", "apks/crash_reporting/acra.apk"),
+    TestedApplication("nosdk", "crash_reporting", "scenarios/crash_reporting/cr_nosdk.sh", 20, "tpl.crashreporting.template", "apks/crash_reporting/nosdk.apk")
 ]
 
 # Time (in seconds) left to the user to boot the phone before measurements start.
