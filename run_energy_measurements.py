96efc52f2d979c46f8bcf341f3eff31162eacfe4
Rémy Raes <contact@remyraes.com>
2023-07-28 17:24:38 +0200

    feat: use same setup methods in both scripts

--- run_energy_measurements.py
+++ run_energy_measurements.py
@@ -1,9 +1,11 @@
 import subprocess
 import threading
 from time import sleep
+from datetime import datetime
 
 import Monsoon.sampleEngine as sampleEngine
 
+from run_metrics_measurements import setup_metrics, stop_metrics_processus, collect_metrics
 from utils.settings import applications, adb, deviceId, runsCount
 from utils.setup import before_app_experiment, before, after, start_scenario
 from utils.monsoon import setup_monsoon
@@ -24,7 +26,15 @@ def run_all_experiments():
         * run scenarios.
     """
 
+    now = datetime.now()
+    print(now.strftime("Started experiments at %d-%m-%Y %H:%M:%S."))
+
     results_dir = before()
+
+    # Push utility script on phone
+    print("\n==> Push utility script on phone...")
+    subprocess.call("{} -s {} push {} /data/local/tmp".format(adb, deviceId, "utils/scripts/runcommand.sh"), shell=True)
+
 
     # Run scenarios
     for x in range(runsCount):
@@ -32,12 +42,11 @@ def run_all_experiments():
             before_app_experiment()
             print("\n==> Launching run n°{} with {} application".format(x, app.name))
 
-            # Install application
-            # print("====> Installing test application on phone...")
-            # subprocess.call("{} -s {} install {}".format(adb, deviceId, app.apk_path), shell=True)
+            pids = setup_metrics(app.package_name)
 
             # Stops sampling after scenario is over.
             monsoon_engine.setStopTrigger(sampleEngine.triggers.GREATER_THAN, app.duration)
+            print("====> Waiting for scenario to end...")
 
             # Launch scenario
             threading.Thread(target=start_scenario, args=(app.scenario,)).start()
@@ -58,9 +67,14 @@ def run_all_experiments():
             # Stop the application.
             subprocess.call("{} -s {} shell am force-stop {}".format(adb, deviceId, app.package_name), shell=True)
 
-            # Uninstall application
-            # print("====> Uninstalling test application...")
-            # subprocess.call("{} -s {} uninstall {}".format(adb, deviceId, app.package_name), shell=True)
+            # Stop metrics sampling on the phone
+            stop_metrics_processus(pids)
+
+            # Waiting some time to wait for metrics sampling to end
+            sleep(3)
+
+            # Download metrics and remove associated files from tested phone.
+            collect_metrics("{}/{}/{}_{}".format(results_dir, app.category, app.name.replace(" ", "_"), x))
 
     after()
 
@@ -71,5 +85,6 @@ def start_sampling():
 
 # Prepare Monsoon tooling
 monsoon_engine = setup_monsoon()
+
 run_all_experiments()
 exit(0)
